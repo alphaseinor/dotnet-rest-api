b5a2dc0d85d748837a10357a9f6924f5e26fff43
Brian Hague <alphaseinor@gmail.com>
2021-01-10 21:44:16 -0600

    implemented interface versus class using addscoped lifecycle

--- Jokes/Startup.cs
+++ Jokes/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Jokes.Interfaces;
 
 namespace example
 {
@@ -26,6 +27,7 @@ namespace example
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IJokes, MockJoke>();
         }
 
         // this will be used for middleware/requests
